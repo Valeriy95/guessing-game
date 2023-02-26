af923013aca5fa703d752b789ca05818aaa727ad
Valeriy95 <vmariukhnyk@gmail.com>
2023-02-26 15:44:26 +0400

    feat: completed

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,31 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor(value) {
+        this.root = value;
+        this.left = null;
+        this.right = null;
+    }
+    
     setRange(min, max) {
-
+        this.left = min;
+        this.right = Math.round(max / 2);
     }
-
+  
     guess() {
-
+        return this.right;
     }
-
+  
     lower() {
-
+        this.right = Math.round((this.right - this.left) / 2 + this.left);
+        this.guess();
     }
-
+  
     greater() {
-
+        let a = Math.round((this.right - this.left) / 2 + this.right);
+        this.left = this.right;
+        this.right = a;
+        this.guess()
     }
 }
+
 
 module.exports = GuessingGame;
